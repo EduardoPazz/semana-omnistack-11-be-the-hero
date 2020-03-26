667b942a43c1a8ad65083c75ccd2584ba7617ee2
Eduardo da Paz <jooeduardo8@gmail.com>
2020-03-26 18:22:12 -0300

    Linking routes to database

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const routes = express.Router();
 
-routes.get('/example', (req, res) => {
-    res.send("It worked!");
+const connection = require('./database/connection.js');
+
+routes.get('/ongs', async (req, res) => {
+    const data = await connection('ongs').select('*');
+
+    return res.json(data);
+});
+
+routes.post('/ongs', async (req, res) => {
+     const { name, email, whatsapp, city, uf } = req.body;
+     const id = require('crypto').randomBytes(4).toString('HEX'); // create a random id
+
+    
+    await connection('ongs').insert({
+        id, name, email, whatsapp, city, uf
+    });
+
+     return res.json({ id });
 });
 
 module.exports = routes;
