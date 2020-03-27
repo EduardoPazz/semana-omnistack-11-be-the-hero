c02fefe884d73aeb3fd1b6e8118ff67f90bf7d3f
Eduardo da Paz <jooeduardo8@gmail.com>
2020-03-27 13:19:05 -0300

    Adding CORS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+
 const routes = require('./routes.js');
 
-app.use(express.json());
+const cors = require('cors');
 
+app.use(cors());
+
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
